e378fb3344e23124f1c0863562a1c1738757afab
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-07-06 11:33:33 +0100

    added a new feature

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) =>{
-    res.send("<h1>Welcome to SCA Cloud School Application</h1>")
+    res.send("<h1>Welcome to SCA Cloud School Application , this is my first assessment</h1>")
 })
 
 app.listen(3000, () =>{
